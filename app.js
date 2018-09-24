6e65c95bbe367fbe0f4daec034fd3149deb24b65
limaleandro1999 <limaleandro1999@gmail.com>
2018-09-24 16:51:43 -0300

    teste no schedule

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ var port = process.env.PORT || '3000';
 app.listen(port);
 console.log('Servidor rodando!');
 
-var j = schedule.scheduleJob('40 9 * * 0,1-5', function(){
+var j = schedule.scheduleJob('00 17 * * 0,1-5', function(){
     localizadorDeEstagio();
+});
+
+var k = schedule.scheduleJob('/1 * * * * 0,1-5', function(){
+    var date = new Date();
+    console.log(date.getHours() + ':' + date.getMinutes())
 });
